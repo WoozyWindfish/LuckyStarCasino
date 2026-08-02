57531ba4e02010247cf37729f0d2fb5d6c410072
Nic <nicenglehardt@gmail.com>
2026-08-02 09:44:11 +0900

    Display starting balance

--- src/com/nic/luckystarcasino/CasinoApplication.java
+++ src/com/nic/luckystarcasino/CasinoApplication.java
@@ -5,5 +5,11 @@ public class CasinoApplication {
     static void main(String[] args) {
         System.out.println("=== Lucky Star Casino ===");
         System.out.println("Welcome to the Lucky Star Casino Partner!");
+
+        int balance = 1000;
+
+        System.out.println("Balance =  $" + balance);
+
     }
+
 }
