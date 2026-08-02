a2324c2887e00a0fa3a8ce1fc92227d7af243a69
Nic <nicenglehardt@gmail.com>
2026-08-02 09:55:34 +0900

    Add menu options and scanner utility.

--- src/com/nic/luckystarcasino/CasinoApplication.java
+++ src/com/nic/luckystarcasino/CasinoApplication.java
@@ -1,14 +1,25 @@
 package com.nic.luckystarcasino;
 
+import java.util.Scanner;
+
 public class CasinoApplication {
 
     static void main(String[] args) {
-        System.out.println("=== Lucky Star Casino ===");
-        System.out.println("Welcome to the Lucky Star Casino Partner!");
+
+        new Scanner(System.in);
 
         int balance = 1000;
 
+
+        System.out.println("=== Lucky Star Casino ===");
+        System.out.println("Welcome to the Lucky Star Casino Partner!");
         System.out.println("Balance =  $" + balance);
+
+        System.out.println("1: Play slots");
+        System.out.println("2: Quit");
+        System.out.println("Please select");
+
+        int choice = scanner.nextInt();
 
     }
 
