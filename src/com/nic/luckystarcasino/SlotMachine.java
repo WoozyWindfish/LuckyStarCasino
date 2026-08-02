51bc51c3b359cf0f5a9b52fc38e7ec92a40ac1e7
Nic <nicenglehardt@gmail.com>
2026-08-02 12:43:22 +0900

    Create welcome message and create array for slot rows. Implement spin and random number utility.

--- src/com/nic/luckystarcasino/SlotMachine.java
+++ src/com/nic/luckystarcasino/SlotMachine.java
@@ -1,4 +1,71 @@
 package com.nic.luckystarcasino;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class SlotMachine {
+
+    static void main(String[] args) {
+
+        //  JAVA SLOT MACHINE
+        Scanner scanner = new Scanner(System.in);
+        int balance = 1000;
+        int bet;
+        int payout;
+        String[] row;
+
+        //  DISPLAY WELCOME MESSAGE (Welcome to Sarsaparilla Slots!)
+        System.out.println("************************************");
+        System.out.println("   Welcome to Sarsaparilla Slots!   ");
+        System.out.println("      Symbols: 🍾 🤠 🐎 🌟 🔫");
+        System.out.println("**************************************");
+
+
+        //  PLAY IF BALANCE >0
+
+        while (balance > 0) {
+            System.out.println("Current balance: $" + balance);
+            System.out.print("Place your bet amount: ");
+            bet = scanner.nextInt();
+
+            if (bet > balance) {
+                System.out.println("INSUFFICIENT FUNDS");
+                continue;
+            } else if (bet <= 0) {
+                System.out.println("Bet must be greater than 0");
+            } else {
+                balance -= bet;
+                System.out.println("$" + balance);
+            }
+
+            //  SPIN ROW
+            System.out.println("Spinning...");
+            spinRow();
+
+
+            //  PRINT ROW OF EMOJIS
+            //  GET A PAYOUT
+            //  ASK TO PLAY AGAIN
+            //  DISPLAY EXIT MESSAGE
+
+        }
+        scanner.close();
+    }
+        static String[] spinRow() {
+
+            String[] symbols = {"🍾", "🤠", "🐎", "🌟", "🔫"};
+            String[] row = new String[3];
+            Random random = new Random();
+
+
+
+            for(int i = 0; i < 3; i++) {
+                row[i] = symbols[random.nextInt(symbols.length)];
+            }
+
+            System.out.println(row[0] + row[1] + row[2]);
+
+
+            return new String[0];
+        }
 }
